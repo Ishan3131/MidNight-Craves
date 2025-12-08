ffc034472fa12ab6c41776691e4e40d1798eb1ef
Ishan3131 <chetwanikrish@gmail.com>
2025-12-09 02:12:16 +0530

    changed few designs, added cart section with initial working functions

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ let overlay = document.getElementById('overlay');
 let cartBtn = document.getElementById('cartBtn');
 let cardsContainer = document.getElementById('food-cards-container');
 let removeCartSection = document.getElementById('remove-cart-section-btn')
+
 function cart() {
     cartSection.style.transform = 'translateX(0)';
     overlay.style.display = 'block';
@@ -19,3 +20,41 @@ function cartRemove() {
 
 cartBtn.addEventListener('click', cart);
 removeCartSection.addEventListener('click', cartRemove);
+
+function reduce(btn) {
+    let card = btn.parentElement.parentElement.parentElement;
+    let quantitySpan = card.querySelector('.item-quantity-number');
+    let price = Number(card.querySelector('.item-price-number').innerText)
+    if(quantitySpan.innerText > 1){
+        quantitySpan.innerText -= 1;
+        card.querySelector('.item-total-price').innerText -= price;
+        document.getElementById('totalItems').innerText--;
+        document.getElementById('grandTotal').innerText -= price;
+    }
+}
+
+function add(btn){
+    let card = btn.parentElement.parentElement.parentElement;
+    let price = Number(card.querySelector('.item-price-number').innerText)
+    card.querySelector('.item-quantity-number').innerText++;
+    card.querySelector('.item-total-price').innerText = Number(card.querySelector('.item-total-price').innerText) + price;
+    document.getElementById('totalItems').innerText++;
+    document.getElementById('grandTotal').innerText = Number(document.getElementById('grandTotal').innerText) + price;
+}
+
+function removeItem(btn) {
+    let card = btn.parentElement.parentElement;
+    let totalItems = document.getElementById('totalItems')
+    document.getElementById('grandTotal').innerText -= Number(card.querySelector('.item-total-price').innerText);
+    totalItems.innerText -= Number(card.querySelector('.item-quantity-number').innerText);
+    card.innerHTML = ''
+    card.classList.add('remove');
+    setTimeout(()=>card.remove(), 500);
+    if(totalItems.innerText == 0){
+        setTimeout(function (){
+        document.getElementById('cart-list').style.display = 'none';
+        document.getElementById('bill').style.display = 'none';
+        document.getElementById('empty-cart-message').style.display = 'flex';
+        }, 500)
+    }
+}
